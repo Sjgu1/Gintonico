4b15bd708877012dd2c92ed7dc476d505db591a8
LarryRider <lriderg@gmail.com>
2018-02-28 21:51:31 +0100

    Pequeños cambios servidor

--- servidor/server.go
+++ servidor/server.go
@@ -10,7 +10,15 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there!")
+}
 
+func handlerLogin(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hi there!")
+}
+
+func handlerRegister(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there!")
 }
 
 func redirectToHTTPS(w http.ResponseWriter, r *http.Request) {
@@ -23,16 +31,20 @@ func main() {
 	err := httpscerts.Check("cert.pem", "key.pem")
 	// If they are not available, generate new ones.
 	if err != nil {
-		err = httpscerts.Generate("cert.pem", "key.pem", "127.0.0.1:8081")
+		err = httpscerts.Generate("cert.pem", "key.pem", ":8081")
 		if err != nil {
 			log.Fatal("Error: Couldn't create https certs.")
 		}
 	}
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", handlerLogin)
+	http.HandleFunc("/register", handlerRegister)
 
 	// Start the HTTPS server in a goroutine
 	go http.ListenAndServeTLS(":8081", "cert.pem", "key.pem", nil)
 	// Start the HTTP server and redirect all incoming connections to HTTPS
 	err2 := http.ListenAndServe(":8080", http.HandlerFunc(redirectToHTTPS))
-	log.Fatal(err2)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
 }
