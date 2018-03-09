b59e4db237aeaffe5cc3a89a7e9f92d47aa7e3b7
LarryRider <lriderg@gmail.com>
2018-03-09 21:16:12 +0100

    Añadido icono y cliente rediseñado

--- cliente/main.go
+++ cliente/main.go
@@ -11,6 +11,7 @@ import (
 )
 
 var body *gowd.Element
+var mostrar = "login"
 
 // función para comprobar errores (ahorra escritura)
 func check(e error) {
@@ -32,8 +33,33 @@ func sendServerPetition(data map[string][]string, route string) *http.Response {
 
 func main() {
 	body = bootstrap.NewContainer(false)
-	body.AddHTML(`<div class="container">
-    	<div class="row">
+
+	body.AddHTML(`<div style="margin:0 auto;width:40%;"><img src="img/logo.png" style="width:100%;margin:0 auto"/></div><br/><br/>`, nil)
+
+	switch mostrar {
+	case "login":
+		body.AddHTML(vistaLogin(), nil)
+		body.Find("login-submit").OnEvent(gowd.OnClick, sendLogin)
+		body.Find("register-form-link").OnEvent(gowd.OnClick, mostrarRegister)
+		body.Find("login-form-link").OnEvent(gowd.OnClick, mostrarLogin)
+		break
+	case "register":
+		body.AddHTML(vistaRegister(), nil)
+		body.Find("register-submit").OnEvent(gowd.OnClick, sendRegister)
+		body.Find("login-form-link").OnEvent(gowd.OnClick, mostrarLogin)
+		body.Find("register-form-link").OnEvent(gowd.OnClick, mostrarRegister)
+		break
+	}
+
+	//start the ui loop
+	err := gowd.Run(body)
+
+	check(err)
+}
+
+func vistaLogin() string {
+	return `<div class="container">
+		<div class="row">
 			<div class="col-md-6 col-md-offset-3">
 				<div class="panel panel-login">
 					<div class="panel-heading">
@@ -50,35 +76,17 @@ func main() {
 					<div class="panel-body">
 						<div class="row">
 							<div class="col-lg-12">
-								<div id="login-form" style="display: block;">
+								<div id="login-form">
 									<div class="form-group">
-										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+										<input type="text" id="usuario" class="form-control" placeholder="Usuario" autocomplete="off">
 									</div>
 									<div class="form-group">
-										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-									</div>
-									<div class="form-group">
-										<div class="row">
-											<div class="col-sm-6 col-sm-offset-3">
-												<button name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login">Iniciar Sesión</button>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div id="register-form" style="display: none;">
-									<div class="form-group">
-										<input type="text" name="registerUser" id="registerUser" tabindex="1" class="form-control" placeholder="Username" value="">
-									</div>
-									<div class="form-group">
-										<input type="password" name="registerPassword" id="registerPassword" tabindex="2" class="form-control" placeholder="Password">
-									</div>
-									<div class="form-group">
-										<input type="password" name="confirmPassword" id="confirmPassword" tabindex="2" class="form-control" placeholder="Confirm Password">
+										<input type="password" id="contraseña" class="form-control" placeholder="Contraseña" autocomplete="off">
 									</div>
 									<div class="form-group">
 										<div class="row">
 											<div class="col-sm-6 col-sm-offset-3">
-												<button name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register">Regístrate ya!</button>
+												<button id="login-submit" class="form-control btn btn-login">Iniciar Sesión</button>
 											</div>
 										</div>
 									</div>
@@ -90,21 +98,72 @@ func main() {
 			</div>
 		</div>
 	</div>
-	<p id="texto"/>`, nil)
-
-	body.Find("login-submit").OnEvent(gowd.OnClick, btnLogin)
-	body.Find("register-submit").OnEvent(gowd.OnClick, btnRegister)
-
-	//start the ui loop
-	gowd.Run(body)
+	<p id="texto"/>`
 }
 
-func btnLogin(sender *gowd.Element, event *gowd.EventElement) {
+func vistaRegister() string {
+	return `<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="panel panel-login">
+					<div class="panel-heading">
+						<div class="row">
+							<div class="col-xs-6">
+								<a id="login-form-link" href="#">Login</a>
+							</div>
+							<div class="col-xs-6">
+								<a id="register-form-link" href="#" class="active">Register</a>
+							</div>
+						</div>
+						<hr>
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-lg-12">
+								<div id="register-form">
+									<div class="form-group">
+										<input type="text" id="registerUser" class="form-control" placeholder="Username" autocomplete="off">
+									</div>
+									<div class="form-group">
+										<input type="password" id="registerPassword" class="form-control" placeholder="Password" autocomplete="off">
+									</div>
+									<div class="form-group">
+										<input type="password" id="confirmPassword" class="form-control" placeholder="Confirm Password" autocomplete="off">
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-sm-6 col-sm-offset-3">
+												<button id="register-submit" class="form-control btn btn-register">Regístrate ya!</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<p id="texto"/>`
+}
+
+func mostrarLogin(sender *gowd.Element, event *gowd.EventElement) {
+	mostrar = "login"
+	main()
+}
+
+func mostrarRegister(sender *gowd.Element, event *gowd.EventElement) {
+	mostrar = "register"
+	main()
+}
+
+func sendLogin(sender *gowd.Element, event *gowd.EventElement) {
 
 	// ** ejemplo de registro
 	data := url.Values{} // estructura para contener los valores
-	data.Set("login", body.Find("username").GetValue())
-	data.Set("password", body.Find("password").GetValue())
+	data.Set("login", body.Find("usuario").GetValue())
+	data.Set("password", body.Find("contraseña").GetValue())
 
 	response := sendServerPetition(data, "/login")
 
@@ -115,7 +174,7 @@ func btnLogin(sender *gowd.Element, event *gowd.EventElement) {
 	body.Find("texto").SetText(s)
 }
 
-func btnRegister(sender *gowd.Element, event *gowd.EventElement) {
+func sendRegister(sender *gowd.Element, event *gowd.EventElement) {
 
 	// ** ejemplo de registro
 	data := url.Values{} // estructura para contener los valores
